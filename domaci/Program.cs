05a8e4be8d9913af1055e11f54d8e39413103d50
SodooSodastreamSodny <chingunsodoo786@gmail.com>
2023-10-07 12:48:58 +0200

    Add project files.

---
+++ domaci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Pokemon pokemon1 = new Pokemon("fire", "second");
+            Console.WriteLine(pokemon1.ChoosePokemon()); 
+        }
+    }
+}
